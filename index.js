ed6d8b9d34b4a91082d55d43dc1ecf6f3755e23c
kelldork <klk2@txstate.edu>
2017-03-22 11:13:30 -0400

    pinned tweet popup added

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ var app = electron.app;
 
 
 app.on('ready', function() {
-	//variable for window being created
-	var appWindow;
+	//variable for windows being created
+	var appWindow, infoWindow;
+	//can pass an object with settings BrowserWindow(IN HERE);
 	appWindow = new BrowserWindow();
 	appWindow.loadURL('http://twitter.com/kasiekelldorf');
+
+	infoWindow = new BrowserWindow({
+		//width: 400,
+		//height: 300,
+		//transparent: true,
+		frame: false
+	});
+
+	//infoWindow.loadURL('file://' + __dirname + '/info.html');
+	infoWindow.loadURL('https://twitter.com/kasiekelldorf/status/753238826890342400');
 });
